60bbfa877a72c1a2bdc21cb79f7df7388cc2d5fc
GVenusLeo <gvenusleo@gmail.com>
2022-09-19 09:25:18 +0800

    初始化

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,56 @@
+export default {
+  language: "zh-CN",
+  title: "应许之地",
+  titleTemplate: "liuyu.xin 的 wiki",
+  description: "liuyu.xin 的 wiki：技术学习、阅读思考、工具技巧",
+  appearance: true,
+  base: "/",
+  ignoreDeadLinks: false,
+  lastUpdated: true,
+  head: [["link", { rel: "icon", href: "/liuyuxin.png" }]],
+  themeConfig: {
+    siteTitle: "应许之地",
+    logo: "/liuyuxin.png",
+    lastUpdatedText: "更新",
+    async transformHead(ctx) {},
+    outline: [2, 3],
+    outlineTitle: "文章大纲",
+    socialLinks: [
+      { icon: "twitter", link: "https://twitter.com/liuyu_xin" },
+      { icon: "github", link: "https://github.com/gvenusleo" },
+    ],
+    docFooter: {
+      prev: "上一篇",
+      next: "下一篇",
+    },
+    footer: {
+      message: "Released under the CC BY-NC-SA 4.0 License.",
+      copyright: "Copyright © 2022 liuyu.xin",
+    },
+    nav: [
+      {
+        text: "关于",
+        link: "/about/",
+      },
+      {
+        text: "CS 基础",
+        items: [{ text: "数据结构", link: "/cs-base/data-structure/01" }],
+      },
+    ],
+    sidebar: {
+      "/cs-base/data-structure/": [
+        {
+          text: "数据结构",
+          items: [
+            { text: "01 - 数据结构导论", link: "/cs-base/data-structure/01" },
+          ]
+        }
+      ]
+    },
+  },
+  markdown: {
+    config: (md) => {
+      md.use(require("markdown-it-katex"));
+    },
+  },
+};
---
+++ docs/index.md
@@ -0,0 +1,26 @@
+---
+layout: home
+
+hero:
+  name: 应许之地
+  text: liuyu.xin 的 wiki
+  tagline: 技术学习、阅读思考、工具技巧
+  actions:
+    - theme: brand
+      text: 订阅周报
+      link: https://promiseland.zhubai.love
+    - theme: alt
+      text: 联系我
+      link: https://muselink.cc/gvenusleo
+
+features:
+  - icon: 
+    title: 技术学习
+    details: 写写技术学习笔记
+  - icon: 
+    title: 阅读思考
+    details: 记录读过的好书与收获
+  - icon: 
+    title: 工具技巧
+    details: 分享一些有趣有用的工具与技巧
+---
